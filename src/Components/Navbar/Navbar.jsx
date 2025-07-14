541dd28aeac0db5562aeb6d182d35e29c3ca1602
unknown <trizahnabwire20@gmail.com>
2025-07-14 23:44:58 +0300

    Navbar Component

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/Trizah Logo.png'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <img src={logo} alt='Logo' className='navbar-logo' />
+        <ul className="nav-menu">
+          <li>Home</li>
+          <li>About Me</li>
+          <li>Services</li>
+          <li>Portfolio</li>
+          <li>Contact</li>
+        </ul>
+        <div className="nav-connect">
+          Connect With Me
+        </div>
+
+    </div>
+  )
+}
+
+export default Navbar
