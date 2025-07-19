55fc5bd1bf026039c9d565b8156c9c4e68cef98f
unknown <trizahnabwire20@gmail.com>
2025-07-19 12:11:57 +0300

    Services

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import './Services.css'
+import Services_Data from '../../assets/services_data.js'
+import arrow_icon from '../../assets/arrow_icon.svg'
 
 const Services = () => {
   return (
-    <div>
+    <div className='services'>
+      <div className="services-title">
+        <h1>My Services</h1>
+      </div>
+      <div className="services-container">
+        {Services_Data.map((service, index)=>{
+          return <div key={index} className="service-format">
+            <h3>{service.$_no}</h3>
+            <h2>{service.$_name}</h2>
+            <p>{service.$_desc}</p>
+            <div className="services_readmore">
+              <p>Read More</p>
+              <img src={arrow_icon} alt="arrow icon" />
+            </div>
+          </div>
+        })}
+
+      </div>
 
     </div>
   )
---
+++ src/assets/services_data.js
@@ -0,0 +1,34 @@
+const Services_Data = [
+  {
+    $_no: "01",
+    $_name: "User Research",
+    $_desc: "Conducting user research to understand needs and behaviors.",
+  },
+    {
+    $_no: "02",
+    $_name: "Wireframing",
+    $_desc: "Creating wireframes to visualize the layout and structure.",
+  },
+    {
+    $_no: "03",
+    $_name: "Prototyping",
+    $_desc: "Developing interactive prototypes for user testing.",
+  },
+    {
+    $_no: "04",
+    $_name: "Usability Testing",
+    $_desc: "Testing designs with real users to gather feedback.",
+    },
+        {
+    $_no: "05",
+    $_name: "Visual Design",
+    $_desc: "Crafting visually appealing designs that align with brand identity.",
+    },
+        {
+    $_no: "06",
+    $_name: "Accessibility Design",
+    $_desc: "Ensuring designs are accessible to all users, including those with disabilities.",
+    },
+];
+
+export default Services_Data;
