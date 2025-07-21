c1fd5fce6fc251c95bbef38eb67eccbddbd30ae4
unknown <trizahnabwire20@gmail.com>
2025-07-21 08:34:22 +0300

    MyWork Component

---
+++ src/assets/mywork_data.js
@@ -0,0 +1,29 @@
+import project1 from '../assets/project1.png';
+import project2 from '../assets/project2.png';
+import project3 from '../assets/project3.png';
+
+const mywork_data = [
+  {
+    w_no: 1,
+    w_title: "Couples&Money Redesign",
+    w_desc: "Empowering couples with the knowledge and skills necessary to make informed financial decisions.",
+    w_img: project1,
+    w_link: "https://couplesnmoney.com/"
+    },
+    {
+    w_no: 2,
+    w_title: "GigsMtaani",
+    w_desc: "Connecting Kenyan youth with local informal casual gig opportunities to enhance their skills and financial independence.",
+    w_img: project2,
+    w_link: "https://www.behance.net/gallery/224778933/GigsMtaani-Casual-Local-Gigs-Near-You"
+    },
+    {
+    w_no: 3,
+    w_title: "Early African Chriastinity",
+    w_desc: "A redesign of the website for Early African Christianity, focusing on the rich history and contributions of African Christians.",
+    w_img: project3,
+    w_link: "https://earlyafricanchristianity.com/"
+    },
+]
+
+export default mywork_data;
