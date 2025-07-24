5f5f3469f2c0c7bf71e6d714b7724f789c6dd202
unknown <trizahnabwire20@gmail.com>
2025-07-24 18:14:26 +0300

    Underline active menu

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import './Navbar.css'
 import logo from '../../assets/Trizah Logo.png'
+import { useState } from 'react'
 
 const Navbar = () => {
+
+  const [menu,setMenu] = useState('home')
   return (
     <div className='navbar'>
         <img src={logo} alt='Logo' className='navbar-logo' />
         <ul className="nav-menu">
-          <li>Home</li>
-          <li>About Me</li>
-          <li>Services</li>
-          <li>Portfolio</li>
-          <li>Contact</li>
+          <li><p onClick={()=>setMenu("home")}>Home</p>{menu==="home"?<div className='underline'></div>:<></>}</li>
+          <li><p onClick={()=>setMenu("about")}>About Me</p>{menu==="about"?<div className='underline'></div>:<></>}</li>
+          <li><p onClick={()=>setMenu("services")}>Services</p>{menu==="services"?<div className='underline'></div>:<></>}</li>
+          <li><p onClick={()=>setMenu("work")}>Portfolio</p>{menu==="work"?<div className='underline'></div>:<></>}</li>
+          <li><p onClick={()=>setMenu("contact")}>Contact</p>{menu==="contact"?<div className='underline'></div>:<></>}</li>
         </ul>
         <div className="nav-connect">
           Connect With Me
